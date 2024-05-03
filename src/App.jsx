89cbfbbbd35e082564cfe4f23d8ef747eeb8aebc
to'raboyev xursand <xursandtoraboyev810@gmail.com>
2024-05-03 21:03:17 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1 className="text-8xl text-black"  >Hello</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
